b43c1d6765b6b179cbe8f8c08acb4ad0f1e7b04f
Leonardo Lima <leolimaf@outlook.com>
2021-01-19 15:20:28 -0300

    Saving a new Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,12 +1,18 @@
 package gui;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Labeled;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,6 +20,8 @@ import java.util.ResourceBundle;
 public class DepartmentFormController implements Initializable {
 
     private Department entity;
+
+    private DepartmentService service;
 
     @FXML
     private TextField txtId;
@@ -30,8 +38,12 @@ public class DepartmentFormController implements Initializable {
         this.entity = entity;
     }
 
+    public void setDepartmentService(DepartmentService service) {
+        this.service = service;
+    }
+
     public void updateFormData() {
-        if (entity == null){
+        if (entity == null) {
             throw new IllegalStateException("Entity was null");
         }
         txtId.setText(String.valueOf(entity.getId()));
@@ -39,13 +51,34 @@ public class DepartmentFormController implements Initializable {
     }
 
     @FXML
-    public void onBtnSaveAction() {
-        System.out.println("btnSave");
+    public void onBtnSaveAction(ActionEvent event) {
+        if (entity == null) {
+            throw new IllegalStateException("Entity was null");
+        }
+        if (service == null) {
+            throw new IllegalStateException("Service was null");
+        }
+        try {
+            entity = getFormData();
+            service.SaveOrUpdate(entity);
+            Utils.currentStage(event).close();
+        } catch (DbException e) {
+            Alerts.showAlert("Error saving object", null, e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+
+    private Department getFormData() {
+        Department obj = new Department();
+
+        obj.setId(Utils.tryParseToInteger(txtId.getText()));
+        obj.setName(txtName.getText());
+
+        return obj;
     }
 
     @FXML
-    public void onBtnCancelAction() {
-        System.out.println("btnCancel");
+    public void onBtnCancelAction(ActionEvent event) {
+        Utils.currentStage(event).close();
     }
 
     @Override
