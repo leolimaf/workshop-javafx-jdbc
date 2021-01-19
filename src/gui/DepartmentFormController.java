d89f03fc2eca84de76487a8f75a8ee7ab9ec6445
Leonardo Lima <leolimaf@outlook.com>
2021-01-19 16:39:22 -0300

    Validation Exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -13,12 +13,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Labeled;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class DepartmentFormController implements Initializable {
 
@@ -74,6 +73,8 @@ public class DepartmentFormController implements Initializable {
             Utils.currentStage(event).close();
         } catch (DbException e) {
             Alerts.showAlert("Error saving object", null, e.getMessage(), Alert.AlertType.ERROR);
+        } catch (ValidationException e){
+            setErrorMessages(e.getErrors());
         }
     }
 
@@ -84,8 +85,18 @@ public class DepartmentFormController implements Initializable {
     private Department getFormData() {
         Department obj = new Department();
 
+        ValidationException exception = new ValidationException("Validation Error");
+
         obj.setId(Utils.tryParseToInteger(txtId.getText()));
+
+        if (txtName.getText() == null || txtName.getText().trim().equals("")){
+            exception.addError("name", "Field can't be empty");
+        }
         obj.setName(txtName.getText());
+
+        if (exception.getErrors().size() > 0){
+            throw exception;
+        }
 
         return obj;
     }
@@ -103,6 +114,14 @@ public class DepartmentFormController implements Initializable {
     private void initializeNodes() {
         Constraints.setTextFieldInteger(txtId);
         Constraints.setTextFieldMaxLength(txtName, 30);
+    }
+
+    private void setErrorMessages(Map<String, String> errorMessages){
+        Set<String> fields = errorMessages.keySet();
+
+        if (fields.contains("name")){
+            lblErrorName.setText(errorMessages.get("name"));
+        }
     }
 
 }
