fb0a2bd97a7a30b66f7e973be8f2ef608a14fbdb
Leonardo Lima <leolimaf@outlook.com>
2021-01-20 15:40:11 -0300

    Saving Seller

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -19,6 +19,7 @@ import model.services.DepartmentService;
 import model.services.SellerService;
 
 import java.net.URL;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.*;
@@ -137,11 +138,40 @@ public class SellerFormController implements Initializable {
         }
         obj.setName(txtName.getText());
 
+        if (txtEmail.getText() == null || txtEmail.getText().trim().equals("")){
+            exception.addError("email", "Field can't be empty");
+        }
+        obj.setEmail(txtEmail.getText());
+
+        if (dpBirthDate.getValue() == null){
+            exception.addError("birthDate", "Field can't be empty");
+        } else {
+            Instant instant = Instant.from(dpBirthDate.getValue().atStartOfDay(ZoneId.systemDefault()));
+            obj.setBirthDate(Date.from(instant));
+        }
+
+        if (txtBaseSalary.getText() == null || txtBaseSalary.getText().trim().equals("")){
+            exception.addError("baseSalary", "Field can't be empty");
+        }
+        obj.setBaseSalary(Utils.tryParseToDouble(txtBaseSalary.getText()));
+
+        obj.setDepartment(cbDepartment.getValue());
+
         if (exception.getErrors().size() > 0){
             throw exception;
         }
 
         return obj;
+    }
+
+    private void setErrorMessages(Map<String, String> errorMessages){
+        Set<String> fields = errorMessages.keySet();
+
+        lblErrorName.setText(fields.contains("name") ? errorMessages.get("name") : "");
+        lblErrorEmail.setText(fields.contains("email") ? errorMessages.get("email") : "");
+        lblErrorBaseSalary.setText(fields.contains("baseSalary") ? errorMessages.get("baseSalary") : "");
+        lblErrorBirthDate.setText(fields.contains("birthDate") ? errorMessages.get("birthDate") : "");
+
     }
 
     @FXML
@@ -161,14 +191,6 @@ public class SellerFormController implements Initializable {
         Constraints.setTextFieldMaxLength(txtEmail,60);
         Utils.formatDatePicker(dpBirthDate,"dd/MM/yyyy");
         initializeComboBoxDepartment();
-    }
-
-    private void setErrorMessages(Map<String, String> errorMessages){
-        Set<String> fields = errorMessages.keySet();
-
-        if (fields.contains("name")){
-            lblErrorName.setText(errorMessages.get("name"));
-        }
     }
 
     private void initializeComboBoxDepartment() {
